485b7ffb888a0cc922e753b8657ee7b502a8e63d
HR Bank Development <dev@hrbank.ca>
2025-11-15 13:08:43 +0000

    auto-commit for 93a69d28-ce9e-426f-98e5-880032291778

--- backend/server.py
+++ backend/server.py
@@ -99,6 +99,7 @@ app.include_router(blockchain_credentials.router, prefix="/api", tags=["blockcha
 app.include_router(translation.router, prefix="/api", tags=["translation"])
 app.include_router(partner_logos.router, tags=["partner-logos"])
 app.include_router(calendar.router, tags=["calendar"])
+app.include_router(workforce_management.router, tags=["workforce_management"])
 
 app.add_middleware(
     CORSMiddleware,
