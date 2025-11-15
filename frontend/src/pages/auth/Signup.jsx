dfd72e53c7e651ff63ae369fbd69191932791e77
HR Bank Development <dev@hrbank.ca>
2025-11-15 15:30:09 +0000

    auto-commit for ce6bab09-18bf-45a3-8728-ed78b6bd9ee6

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, useSearchParams } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import { ThemeProvider, useTheme } from '../../contexts/ThemeContext';
 
