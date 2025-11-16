e1c466ee1f80a7f30f4ffb7dac3b7e9257e12cff
HR Bank Development <dev@hrbank.ca>
2025-11-16 02:57:38 +0000

    auto-commit for 176c11e3-9b9a-49da-87a4-69333195dcb2

--- backend/server.py
+++ backend/server.py
@@ -105,6 +105,7 @@ app.include_router(documents.router, tags=["documents"])
 app.include_router(admin_management.router, tags=["admin_management"])
 app.include_router(google_calendar.router, tags=["google_calendar"])
 app.include_router(shift_scheduling.router, tags=["shift_scheduling"])
+app.include_router(admin_occupations.router, tags=["admin_occupations"])
 
 app.add_middleware(
     CORSMiddleware,
