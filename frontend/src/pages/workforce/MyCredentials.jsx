6ee735577449ab2bd0fe0ec686fe1295a921797e
HR Bank Development <dev@hrbank.ca>
2025-11-16 17:30:34 +0000

    auto-commit for 61aa678d-80a9-4c38-bb55-609039c2afd7

--- frontend/src/pages/workforce/MyCredentials.jsx
+++ frontend/src/pages/workforce/MyCredentials.jsx
@@ -65,7 +65,6 @@ const MyCredentials = () => {
       <UserHeader 
         onBackClick={() => navigate('/workforce/dashboard')}
         showBack={true}
-        title="My Credentials"
       />
 
       <main className="max-w-7xl mx-auto px-4 py-8">
