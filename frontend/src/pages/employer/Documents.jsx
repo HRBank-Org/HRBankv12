fd814b4b11f2dddea96928de4accc79eb48c4b2f
HR Bank Development <dev@hrbank.ca>
2025-11-16 15:40:48 +0000

    auto-commit for f091f437-8e6f-44b4-be8f-d46f434a8b15

---
+++ frontend/src/pages/employer/Documents.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DocumentsPage from '../common/DocumentsPage';
+
+const EmployerDocuments = () => {
+  return <DocumentsPage />;
+};
+
+export default EmployerDocuments;
