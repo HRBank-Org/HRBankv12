a178bd6bda9cd0ae77e5f5c6d3160b321da98d99
HR Bank Development <dev@hrbank.ca>
2025-11-16 13:29:14 +0000

    auto-commit for e22179e5-5e5a-4d2c-bf7e-5ebdb6228723

--- frontend/src/pages/admin/ManageAdmins.jsx
+++ frontend/src/pages/admin/ManageAdmins.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import UserHeader from '../../components/common/UserHeader';
 import api from '../../utils/api';
 
 const ManageAdmins = () => {
