9f7a3b7eb007c6f96264c61c61c7ddf71db74883
HR Bank Development <dev@hrbank.ca>
2025-11-16 13:24:47 +0000

    auto-commit for 4405c5d8-e9ed-4835-90a4-731696b44696

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -31,7 +31,7 @@ async def get_current_user_profile(
     elif user_type == "institution":
         profile = await db.institution_profiles.find_one({"institution_id": user_id}, {"_id": 0})
     elif user_type == "admin":
-        profile = await db.admin_profiles.find_one({"user_id": user_id}, {"_id": 0})
+        profile = await db.admin_profiles.find_one({"admin_id": user_id}, {"_id": 0})
     
     return {
         "success": True,
