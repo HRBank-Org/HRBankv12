10b04a3d5332bd4b283e473e05854acdffc0de80
HR Bank Development <dev@hrbank.ca>
2025-11-16 13:37:42 +0000

    auto-commit for 69f9c38d-d06a-46b1-a1f0-f3872347cac6

--- frontend/src/pages/employer/Dashboard.jsx
+++ frontend/src/pages/employer/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { useTheme } from '../../contexts/ThemeContext';
 import api from '../../utils/api';
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
+import UserHeader from '../../components/common/UserHeader';
 
 const EmployerDashboard = () => {
   const { user, logout } = useAuth();
