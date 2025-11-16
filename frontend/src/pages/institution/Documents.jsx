7a523d337436710c07ae5e3db1c71e7e39660d93
HR Bank Development <dev@hrbank.ca>
2025-11-16 15:40:53 +0000

    auto-commit for 371edafd-0746-4435-8025-1e500f0d7b9f

---
+++ frontend/src/pages/institution/Documents.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DocumentsPage from '../common/DocumentsPage';
+
+const InstitutionDocuments = () => {
+  return <DocumentsPage />;
+};
+
+export default InstitutionDocuments;
