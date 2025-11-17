e21cf2887924ffee089444e0ba46ba02bd05386a
HR Bank Development <dev@hrbank.ca>
2025-11-17 10:16:41 +0000

    auto-commit for 9e897d28-19e8-49b6-a71f-5f188ea2765c

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-restricted-globals */
+
+// Service Worker for HR Bank PWA
+const CACHE_NAME = 'hrbank-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/main.css',
+  '/static/js/main.js',
+  '/manifest-employer.json',
+  '/manifest-workforce.json',
+  '/manifest-institution.json'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
