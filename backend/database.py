f79e7414eee284bf7d7e9a90ebab32edc25fe49c
HR Bank Development <dev@hrbank.ca>
2025-11-17 12:37:45 +0000

    auto-commit for 2a9f2b16-38d0-48cf-bb1e-c2933c8d55ac

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
+db_name = os.environ.get('DB_NAME', 'hrbank_db')
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db = client[db_name]
 
 async def get_database():
     return db
