8f964dcd4789fd20911f600d5c73ce99336a13b4
HR Bank Development <dev@hrbank.ca>
2025-11-19 09:18:31 +0000

    auto-commit for 9f2ea8e3-27a1-4808-b1c9-ef66ffc0fdbb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -161,6 +161,7 @@ function AppRoutes() {
         <Route path="/workforce/dashboard" element={<ProtectedRoute allowedUserTypes={['workforce']}><WorkforceDashboard /></ProtectedRoute>} />
         <Route path="/workforce/compliance" element={<ProtectedRoute allowedUserTypes={['workforce']}><WorkerComplianceOnboarding /></ProtectedRoute>} />
         <Route path="/workforce/profile" element={<ProtectedRoute allowedUserTypes={['workforce']}><WorkforceProfile /></ProtectedRoute>} />
+        <Route path="/workforce/my-shifts" element={<ProtectedRoute allowedUserTypes={['workforce']}><MyShifts /></ProtectedRoute>} />
         <Route path="/workforce/occupations" element={<ProtectedRoute allowedUserTypes={['workforce']}><OccupationProfiles /></ProtectedRoute>} />
         <Route path="/workforce/occupations/create" element={<ProtectedRoute allowedUserTypes={['workforce']}><CreateOccupationSimplified /></ProtectedRoute>} />
         <Route path="/workforce/occupations/create-old" element={<ProtectedRoute allowedUserTypes={['workforce']}><CreateOccupation /></ProtectedRoute>} />
