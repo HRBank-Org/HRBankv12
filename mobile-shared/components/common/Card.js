c6842b016aee8782c3cd661cc6f848636d75808f
HR Bank Development <dev@hrbank.ca>
2025-11-21 11:27:10 +0000

    auto-commit for 7eb0b923-a41a-4855-9ce6-e5b6c1515672

---
+++ mobile-shared/components/common/Card.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import colors from '../../constants/colors';
+
+const Card = ({
+  children,
+  onPress,
+  style,
+  elevated = true,
+  ...props
+}) => {
+  const cardStyle = [
+    styles.card,
+    elevated && styles.elevated,
+    style,
+  ];
+
+  if (onPress) {
+    return (
+      <TouchableOpacity
+        style={cardStyle}
+        onPress={onPress}
+        activeOpacity={0.7}
+        {...props}
+      >
+        {children}
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={cardStyle} {...props}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: colors.white,
+    borderRadius: 12,
+    padding: 16,
+    marginBottom: 12,
+  },
+  elevated: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+});
+
+export default Card;
