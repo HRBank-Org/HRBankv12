5ecabd765fae0cda72ea71b856a5b87838cfbb4c
HR Bank Development <dev@hrbank.ca>
2025-11-21 05:26:41 +0000

    auto-commit for d9f11b39-4eef-430f-b4b8-d023243d2634

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -189,6 +189,7 @@ function AppRoutes() {
         {/* Employer Routes */}
         <Route path="/employer/onboarding" element={<ProtectedRoute allowedUserTypes={['employer']}><EmployerOnboarding /></ProtectedRoute>} />
         <Route path="/employer/dashboard" element={<ProtectedRoute allowedUserTypes={['employer']}><EmployerDashboard /></ProtectedRoute>} />
+        <Route path="/employer/jobs" element={<ProtectedRoute allowedUserTypes={['employer']}><JobPosting /></ProtectedRoute>} />
         <Route path="/employer/compliance" element={<ProtectedRoute allowedUserTypes={['employer']}><ComplianceOnboarding /></ProtectedRoute>} />
         <Route path="/employer/profile" element={<ProtectedRoute allowedUserTypes={['employer']}><EmployerProfile /></ProtectedRoute>} />
         <Route path="/employer/workplace-setup" element={<ProtectedRoute allowedUserTypes={['employer']}><WorkplaceSetup /></ProtectedRoute>} />
