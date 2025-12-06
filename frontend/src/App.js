d0413568e6c322d36afc3a030d409b8b690e004a
HR Bank Development <dev@hrbank.ca>
2025-12-06 09:13:23 +0000

    auto-commit for 414749e9-2498-4d38-90f8-7c59dc4ff883

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -213,6 +213,7 @@ function AppRoutes() {
         <Route path="/employer/tasks" element={<ProtectedRoute allowedUserTypes={['employer']}><ManageTasks /></ProtectedRoute>} />
         <Route path="/employer/shift-calendar" element={<ProtectedRoute allowedUserTypes={['employer']}><ShiftCalendar /></ProtectedRoute>} />
         <Route path="/employer/shift-scheduler" element={<ProtectedRoute allowedUserTypes={['employer']}><ShiftScheduler /></ProtectedRoute>} />
+        <Route path="/employer/calendar-scheduling" element={<ProtectedRoute allowedUserTypes={['employer']}><CalendarScheduling /></ProtectedRoute>} />
         <Route path="/employer/workforce-management" element={<ProtectedRoute allowedUserTypes={['employer']}><WorkforceManagement /></ProtectedRoute>} />
         <Route path="/employer/rosters" element={<ProtectedRoute allowedUserTypes={['employer']}><RosterManagement /></ProtectedRoute>} />
         <Route path="/employer/rosters/:rosterId" element={<ProtectedRoute allowedUserTypes={['employer']}><RosterDetail /></ProtectedRoute>} />
