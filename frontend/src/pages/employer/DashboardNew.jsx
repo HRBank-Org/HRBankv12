256cf6e1c5d97c21de1a2fb35b8f9ddaf0d5f973
HR Bank Development <dev@hrbank.ca>
2025-12-13 01:43:12 +0000

    auto-commit for 5de0bb94-9a18-42d6-9b4e-84fce726f361

--- frontend/src/pages/employer/DashboardNew.jsx
+++ frontend/src/pages/employer/DashboardNew.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { useTheme } from '../../contexts/ThemeContext';
 import api from '../../utils/api';
 import UserHeader from '../../components/common/UserHeader';
-import { FiUsers, FiCalendar, FiAward, FiDollarSign, FiSun, FiCloud, FiCloudRain, FiStar, FiClock, FiPlus } from 'react-icons/fi';
+import { FiUsers, FiCalendar, FiAward, FiDollarSign, FiSun, FiCloud, FiCloudRain, FiStar, FiClock, FiPlus, FiBriefcase } from 'react-icons/fi';
 import CalendarView from '../../components/scheduling/CalendarView';
 import RatingModal from '../../components/ratings/RatingModal';
 import WorkerDetailModal from '../../components/workforce/WorkerDetailModal';
