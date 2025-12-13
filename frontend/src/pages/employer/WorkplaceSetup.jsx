f7434cafd5a529a8205878bba84ab640781b52fa
HR Bank Development <dev@hrbank.ca>
2025-12-13 10:38:38 +0000

    auto-commit for 70d40641-c3d0-4278-8bd4-bf20f6afc545

--- frontend/src/pages/employer/WorkplaceSetup.jsx
+++ frontend/src/pages/employer/WorkplaceSetup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import { useTheme } from '../../contexts/ThemeContext';
+import GenericHeader from '../../components/layout/GenericHeader';
+import ModernSidebar from '../../components/layout/ModernSidebar';
 import api from '../../utils/api';
 
 const WorkplaceSetup = () => {
