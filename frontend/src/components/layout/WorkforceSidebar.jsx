2b2c29b01e796d4f0b2939146032b539e7f55c59
HR Bank Development <dev@hrbank.ca>
2025-12-15 14:49:27 +0000

    auto-commit for 3397b9eb-5930-4176-8f1a-b7bb4c30d5b9

--- frontend/src/components/layout/WorkforceSidebar.jsx
+++ frontend/src/components/layout/WorkforceSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { FiHome, FiCalendar, FiBriefcase, FiSearch, FiClock, FiFileText, FiMail, FiBell, FiSettings, FiCheckSquare, FiChevronRight } from 'react-icons/fi';
+import { FiHome, FiCalendar, FiBriefcase, FiSearch, FiClock, FiFileText, FiSettings, FiCheckSquare, FiChevronRight, FiAward } from 'react-icons/fi';
 import { useTheme } from '../../contexts/ThemeContext';
 
 const WorkforceSidebar = () => {
