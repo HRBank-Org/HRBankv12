54963979144ebf3548dd293affee7d6bfbc7f395
HR Bank Development <dev@hrbank.ca>
2025-12-17 14:40:44 +0000

    auto-commit for bd0dbfdb-1e35-4f41-a81a-32529a046dde

--- frontend/src/components/scheduling/CalendarView.jsx
+++ frontend/src/components/scheduling/CalendarView.jsx
@@ -11,7 +11,7 @@ import ShiftDetailModal from '../../components/scheduling/ShiftDetailModal';
 import AssignWorkerModal from '../../components/scheduling/AssignWorkerModal';
 import CopyShiftModal from '../../components/scheduling/CopyShiftModal';
 
-const CalendarView = ({ embedded = false, initialWorkplace = 'all' }) => {
+const CalendarView = ({ embedded = false, initialWorkplace = 'all', setupComplete = true }) => {
   const navigate = useNavigate();
   const [viewMode, setViewMode] = useState('roster'); // week, day, month, roster - Default to roster
   const [currentDate, setCurrentDate] = useState(moment());
