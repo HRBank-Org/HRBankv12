1065c0cfa5d4b860369cb690dba5618b49c78c8c
HR Bank Development <dev@hrbank.ca>
2025-12-21 07:53:46 +0000

    auto-commit for 403db00b-6707-4cf4-97fc-cca839115327

---
+++ frontend/src/components/employer/workforce/index.js
@@ -0,0 +1,6 @@
+// Workforce Management Components - Extracted from WorkforceManagement.jsx for better maintainability
+
+export { default as RecordsTable, SortableHeader } from './RecordsTable';
+export { default as RecruitmentPanel } from './RecruitmentPanel';
+export { InviteModalWrapper, TerminateModal, RehireModal } from './WorkforceModals';
+export { ESA_LIMITS, getAvailabilityStatus } from './esaCompliance';
