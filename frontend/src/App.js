c0c7050885e28b92830fbf60474bf64179c96bc0
HR Bank Development <dev@hrbank.ca>
2025-12-25 21:26:29 +0000

    auto-commit for b60f7753-bffa-474d-9fc1-332691de349b

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -182,6 +182,8 @@ function AppRoutes() {
         {/* Public Routes */}
         <Route path="/" element={isSubdomainPortal() ? <SubdomainPortal /> : <LandingPage />} />
         <Route path="/jobs" element={<PublicJobsPage />} />
+        <Route path="/verify/:credentialId" element={<VerifyCredential />} />
+        <Route path="/verify" element={<VerifyCredential />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
