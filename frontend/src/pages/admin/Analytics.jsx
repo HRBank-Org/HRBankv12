210728a8b5e2087c41d7305fa83645089a8b2614
HR Bank Development <dev@hrbank.ca>
2026-01-08 05:39:51 +0000

    auto-commit for 0812890a-ffd7-4e3c-870b-cde43b9c3f1f

--- frontend/src/pages/admin/Analytics.jsx
+++ frontend/src/pages/admin/Analytics.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import GenericHeader from '../../components/layout/GenericHeader';
+import SuperAdminSidebar from '../../components/layout/SuperAdminSidebar';
 import api from '../../utils/api';
 
 const Analytics = () => {
