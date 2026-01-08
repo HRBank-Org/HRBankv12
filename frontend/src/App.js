334874c7b6675ea175539ef771bdb7a23ad6ff56
HR Bank Development <dev@hrbank.ca>
2026-01-08 08:02:34 +0000

    auto-commit for 8ad5574c-3323-4aff-8292-6f4f4e1cdcf6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,6 +92,7 @@ import AdminSettings from './pages/admin/Settings';
 import PrivacyPolicy from './pages/legal/PrivacyPolicy';
 import TermsOfService from './pages/legal/TermsOfService';
 import VerifyCredential from './pages/public/VerifyCredential';
+import VerifiedCareerProfile from './pages/public/VerifiedCareerProfile';
 import WorkforceCalendar from './pages/workforce/WorkforceCalendar';
 import MyShifts from './pages/workforce/MyShifts';
 import MyTasks from './pages/workforce/MyTasks';
