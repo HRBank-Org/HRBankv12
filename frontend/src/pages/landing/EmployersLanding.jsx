e572be469b8f88243d9553eefda799cf6893845e
HR Bank Development <dev@hrbank.ca>
2026-01-10 11:16:34 +0000

    auto-commit for 6148e5a7-56fb-4a8a-9a2e-23458e19f264

--- frontend/src/pages/landing/EmployersLanding.jsx
+++ frontend/src/pages/landing/EmployersLanding.jsx
@@ -24,6 +24,9 @@ const EmployersLanding = () => {
               <Link to="/" className="text-gray-600 hover:text-[#30496d] font-medium">
                 Workforce
               </Link>
+              <Link to="/employers" className="text-[#ff5f00] font-semibold">
+                Employers <span className="text-orange-500 text-xs">(Beta)</span>
+              </Link>
               <Link to="/institutions" className="text-gray-600 hover:text-[#30496d] font-medium">
                 Institutions
               </Link>
