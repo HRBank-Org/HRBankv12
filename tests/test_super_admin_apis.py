5d71e7b69f9d9fcc4239a8bb1836bbbfa491b6b3
HR Bank Development <dev@hrbank.ca>
2026-01-10 14:42:15 +0000

    auto-commit for bf299283-d0d8-4d39-b109-fc19ef7f2082

--- tests/test_super_admin_apis.py
+++ tests/test_super_admin_apis.py
@@ -6,7 +6,7 @@ import pytest
 import requests
 import os
 
-BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://landing-overhaul-1.preview.emergentagent.com')
+BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://leaderboard-revamp-1.preview.emergentagent.com')
 
 # Test credentials
 ADMIN_EMAIL = "qnizami@hrbank.ca"
