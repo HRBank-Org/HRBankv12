724a6433505c8e30c3a14344c253f084870055d6
HR Bank Development <dev@hrbank.ca>
2026-01-10 11:27:03 +0000

    auto-commit for 0d9967d1-c862-48b2-8a8e-04809d7d194f

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -56,14 +56,14 @@ const LandingPage = () => {
               <Link to="/" className="text-[#30496d] font-semibold">
                 Workforce
               </Link>
-              <Link to="/employers" className="text-gray-600 hover:text-[#ff5f00] font-medium">
-                Employers <span className="text-orange-500 text-xs">(Beta)</span>
-              </Link>
               <Link to="/institutions" className="text-gray-600 hover:text-[#30496d] font-medium">
                 Institutions
               </Link>
               <Link to="/leaderboard" className="text-gray-600 hover:text-[#30496d] font-medium">
                 Leaderboard
+              </Link>
+              <Link to="/employers" className="text-gray-600 hover:text-[#ff5f00] font-medium">
+                Employers <span className="text-orange-500 text-xs">(Beta)</span>
               </Link>
               <Link to="/jobs" className="text-gray-600 hover:text-[#30496d] font-medium">
                 Jobs
