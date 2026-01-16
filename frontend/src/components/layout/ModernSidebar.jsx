ddeee8c06000f5ffbcb4a15327651141f473c45a
HR Bank Development <dev@hrbank.ca>
2026-01-16 18:31:35 +0000

    auto-commit for 7d2f821b-4500-4644-8486-14ebf531b468

--- frontend/src/components/layout/ModernSidebar.jsx
+++ frontend/src/components/layout/ModernSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { FiHome, FiCalendar, FiMapPin, FiUsers, FiUserCheck, FiClock, FiDollarSign, FiFileText, FiChevronRight, FiNavigation, FiSun } from 'react-icons/fi';
+import { FiHome, FiCalendar, FiMapPin, FiUsers, FiUserCheck, FiClock, FiDollarSign, FiFileText, FiChevronRight, FiNavigation, FiSun, FiHelpCircle } from 'react-icons/fi';
 import { useTheme } from '../../contexts/ThemeContext';
 
 const ModernSidebar = () => {
@@ -83,6 +83,16 @@ const ModernSidebar = () => {
       label: 'Payroll',
       icon: FiDollarSign,
       path: '/employer/payroll'
+    },
+    {
+      type: 'category',
+      label: 'Support'
+    },
+    {
+      type: 'item',
+      label: 'Help & Support',
+      icon: FiHelpCircle,
+      path: '/employer/support'
     }
   ];
 
