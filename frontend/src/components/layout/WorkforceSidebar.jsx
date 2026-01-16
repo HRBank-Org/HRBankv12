fde9298db3ba3fc21c795a331ec1b864d0655e15
HR Bank Development <dev@hrbank.ca>
2026-01-16 18:31:18 +0000

    auto-commit for 76e07d9d-9fc6-498b-984a-d4fdf354557e

--- frontend/src/components/layout/WorkforceSidebar.jsx
+++ frontend/src/components/layout/WorkforceSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { FiHome, FiCalendar, FiBriefcase, FiSearch, FiClock, FiFileText, FiSettings, FiCheckSquare, FiChevronRight, FiAward, FiDollarSign, FiNavigation, FiSun, FiShare2, FiGlobe } from 'react-icons/fi';
+import { FiHome, FiCalendar, FiBriefcase, FiSearch, FiClock, FiFileText, FiSettings, FiCheckSquare, FiChevronRight, FiAward, FiDollarSign, FiNavigation, FiSun, FiShare2, FiGlobe, FiHelpCircle } from 'react-icons/fi';
 import { useTheme } from '../../contexts/ThemeContext';
 
 const WorkforceSidebar = () => {
@@ -95,6 +95,16 @@ const WorkforceSidebar = () => {
       label: 'Find Jobs',
       icon: FiSearch,
       path: '/workforce/find-jobs'
+    },
+    {
+      type: 'category',
+      label: 'Support'
+    },
+    {
+      type: 'item',
+      label: 'Help & Support',
+      icon: FiHelpCircle,
+      path: '/workforce/support'
     }
   ];
 
