23170cc44c2d8c0efba39db309e413a2fe660294
HR Bank Development <dev@hrbank.ca>
2026-02-02 14:49:43 +0000

    auto-commit for 5f171ebc-1ef6-4f6c-a72c-74b6761f0cb1

--- frontend/src/components/layout/ModernSidebar.jsx
+++ frontend/src/components/layout/ModernSidebar.jsx
@@ -84,6 +84,12 @@ const ModernSidebar = () => {
       path: '/employer/time-off'
     },
     {
+      type: 'item',
+      label: 'Remote Work',
+      icon: FiMonitor,
+      path: '/employer/remote-work'
+    },
+    {
       type: 'category',
       label: 'Finances'
     },
