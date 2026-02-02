2ebc99c5d324b70042c7b807556e92f33a8077aa
HR Bank Development <dev@hrbank.ca>
2026-02-02 15:44:40 +0000

    auto-commit for db88e357-db43-46bb-a782-6d5ca8f135f5

--- tests/test_comprehensive_apis.py
+++ tests/test_comprehensive_apis.py
@@ -6,7 +6,7 @@ import pytest
 import requests
 import os
 
-BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://timeledger.preview.emergentagent.com')
+BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://hr-payroll-ready.preview.emergentagent.com')
 
 # Test credentials for different user types
 CREDENTIALS = {
