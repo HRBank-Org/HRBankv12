b1aa8fe95393b44f231e5e6ea0be623b2ad72596
HR Bank Development <dev@hrbank.ca>
2026-02-05 07:08:22 +0000

    auto-commit for efaef7a5-c021-4a42-a655-8bf82c2a0977

--- frontend/src/pages/admin/ManageOccupations.jsx
+++ frontend/src/pages/admin/ManageOccupations.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SuperAdminSidebar from '../../components/layout/SuperAdminSidebar';
 import api from '../../utils/api';
 
 const ManageOccupations = () => {
