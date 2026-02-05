572048f84e815359d5b0e02718b7c1e1c677357f
HR Bank Development <dev@hrbank.ca>
2026-02-05 11:20:19 +0000

    auto-commit for 6f22599e-1919-4325-9eab-fb17f1573ffc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -487,6 +487,7 @@ function AppRoutes() {
         <Route path="/admin/notification-settings" element={<ProtectedRoute allowedUserTypes={['admin', 'super_admin']}><NotificationSettings /></ProtectedRoute>} />
         <Route path="/admin/invoices" element={<ProtectedRoute allowedUserTypes={['admin', 'super_admin']}><AdminInvoices /></ProtectedRoute>} />
         <Route path="/admin/partners" element={<ProtectedRoute allowedUserTypes={['admin', 'super_admin']}><PartnerManagement /></ProtectedRoute>} />
+        <Route path="/admin/chat" element={<ProtectedRoute allowedUserTypes={['admin', 'super_admin']}><AdminChat /></ProtectedRoute>} />
         
         {/* Donation Success/Cancel Routes */}
         <Route path="/donation/success" element={<DonationSuccess />} />
