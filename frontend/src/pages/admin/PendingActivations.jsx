eb95ea0f4147210501f0acb8b1153cd796a20ca6
HR Bank Development <dev@hrbank.ca>
2026-02-05 10:31:45 +0000

    auto-commit for c672a842-d936-4188-9e09-5b5b2148bd7f

--- frontend/src/pages/admin/PendingActivations.jsx
+++ frontend/src/pages/admin/PendingActivations.jsx
@@ -192,7 +192,7 @@ const PendingActivations = () => {
       <GenericHeader />
       <div className="flex">
         <SuperAdminSidebar />
-        <main className="flex-1 p-6 lg:ml-64">
+        <main className="flex-1 p-6 lg:ml-[260px] pt-20">
           <div className="max-w-7xl mx-auto">
             {/* Header */}
             <div className="flex justify-between items-center mb-6">
