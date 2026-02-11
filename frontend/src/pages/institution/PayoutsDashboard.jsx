ba876019f536c109525a89da97db5dd496228c30
HR Bank Development <dev@hrbank.ca>
2026-02-11 09:48:02 +0000

    auto-commit for df590f9a-7e19-458c-864f-91b53f6cfcb0

--- frontend/src/pages/institution/PayoutsDashboard.jsx
+++ frontend/src/pages/institution/PayoutsDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTheme } from '../../contexts/ThemeContext';
-import UserHeader from '../../components/common/UserHeader';
+import InstitutionLayout from '../../components/layout/InstitutionLayout';
 import api from '../../utils/api';
 import {
   Wallet, DollarSign, Building2, CheckCircle, Clock, AlertCircle,
--- frontend/src/pages/institution/VerificationRequests.jsx
+++ frontend/src/pages/institution/VerificationRequests.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTheme } from '../../contexts/ThemeContext';
-import UserHeader from '../../components/common/UserHeader';
+import InstitutionLayout from '../../components/layout/InstitutionLayout';
 import api from '../../utils/api';
 
 const VerificationRequests = () => {
