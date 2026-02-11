525a1b287783c9d1a6bbbad64db6b39b08e3f8d0
HR Bank Development <dev@hrbank.ca>
2026-02-11 08:57:34 +0000

    auto-commit for 98055238-eff1-4a4f-93e4-3b2548612456

--- frontend/src/pages/institution/Fundraisers.jsx
+++ frontend/src/pages/institution/Fundraisers.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import InstitutionLayout from '../../components/layout/InstitutionLayout';
 import api from '../../utils/api';
 import { 
   FiPlus, 
